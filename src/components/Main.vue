8f3a61f84045cb04750df31ccb9565f577a22982
LucaZanfrisco <lzanfrisco9@gmail.com>
2024-02-10 12:14:31 +0100

    Layout ristoranti pagina home

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,18 +3,26 @@
         <div class="container">
             <h3 class="text-center text-white fs-1 py-5">Ristoranti</h3>
             <ul class="list-unstyled d-flex flex-column gap-3">
-                <li v-for="restaurant in dataRestaurants">
-                    <img :src="restaurant.full_photo_img" alt="Foto Ristorante" class="img-fluid">
-                    <p class="mb-0 fs-4 fw-bold">{{ restaurant.activity_name }}</p>
-                    <div v-for="type in restaurant.types">
-                        {{ type.name }}
-                    </div>
-                    <a href="">Vai</a>
+                <li v-for="restaurant in dataRestaurants" class="restaurant-card">
+                    <a href="" class="flex-column flex-sm-row">
+                        <img :src="dataRestaurants[2].full_photo_img" alt="Foto Ristorante" class="img-fluid">
+                        <div>
+                            <p class="fs-4 fw-bold">{{ restaurant.activity_name }}</p>
+                            <p>{{ restaurant.description }}</p>
+                        <ul class="list-unstyled d-flex gap-3">
+                            <li v-for="type in restaurant.types" class="fw-bold bg-white p-2 text-dark rounded-2">
+                                {{ type.name }}
+                            </li>
+                        </ul>
+                        </div>
+
+                    </a>
                 </li>
             </ul>
         </div>
     </main>
 </template>
+
 <script>
 import axios from 'axios';
 export default {
@@ -44,65 +52,60 @@ main {
     background-color: black;
 }
 
-li {
-    display: flex;
-    padding: 30px;
-    gap: 3rem;
-    align-items: center;
-
-    a {
-        text-decoration: none;
-        color: inherit;
-        font-size: 32px;
-
-        &:hover {
-            scale: 1.04;
-        }
-    }
-}
-
-li:nth-child(odd) {
-    background-color: #FFCF0E;
+.restaurant-card {
     color: white;
+    text-decoration: none;
     border-radius: 25px;
+
+    &:hover {
+        scale: 1.03;
+    }
 
     img {
         border-image: 15px;
-        height: 300px;
+        height: auto;
         object-fit: cover;
         border-radius: .9375rem;
     }
 
     a {
-        margin-left: auto;
-
-        &::after {
-            content: '\2192';
-        }
-
+        display: flex;
+        text-decoration: none;
+        color: inherit;
+        align-self: center;
+        gap: 30px;
+        padding: 30px;        
     }
 }
 
-li:nth-child(even) {
-    background-color: #FF5757;
-    justify-content: flex-end;
-    border-radius: 25px;
+.restaurant-card:nth-child(odd) {
+    background-color: #FFCF0E;
 
+    a {
+        justify-content: flex-start;
+        &:hover::after {
+            content: '\2192';
+            align-self: center;
+            justify-self: flex-end;
+            font-size: 30px;
+        }
+    }
+}
+
+.restaurant-card:nth-child(even) {
+    background-color: #FF5757;
     img {
         order: 2;
-        border-radius: 15px;
-        background-color: blue;
-        height: 300px;
-        padding: 30px;
     }
 
     a {
-        order: -1;
-        margin-right: auto;
-
-        &::before {
+        justify-content: flex-end;
+        text-align: end;
+        &:hover::before {
             content: '\2190';
+            align-self: center;
+            justify-self: flex-end;
+            font-size: 30px;
         }
     }
-}
-</style>
+}</style>
